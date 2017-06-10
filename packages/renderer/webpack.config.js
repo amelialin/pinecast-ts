a5017544b740a5eaf4e0be94f68e0ac874164e47
Matt Basta <me@mattbasta.com>
2017-06-09 19:26:18 -0700

    Fix gitignore

---
+++ packages/renderer/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+
+const webpack = require('webpack');
+
+
+module.exports = {
+    devtool: 'source-maps',
+    entry: {
+        app: ['./src/index.ts'],
+    },
+    resolve: {
+        mainFields: [
+            'jsnext:main',
+            'module',
+            'main',
+        ],
+        modules: [
+            'node_modules',
+        ],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    cache: false,
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'index.js',
+        libraryTarget: 'commonjs2',
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({minimize: true}),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'awesome-typescript-loader',
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader',
+            },
+        ],
+    },
+    externals: [
+        function(context, request, callback) {
+            switch (request) {
+                case 'bluebird':
+                case 'any-promise':
+                    return callback(null, 'Promise');
+                default:
+                    if (request[0] === '.' || request.includes('@pinecast') && request.search(/@pinecast\/.*\/node_modules\//) === -1) {
+                        return callback();
+                    }
+                    // return callback();
+                    return callback(null, request);
+            }
+        }
+    ],
+    target: 'node',
+    node: {
+        Buffer: false,
+        setImmediate: false,
+        process: false,
+    },
+};
