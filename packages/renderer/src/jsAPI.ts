72c05775282702d82e358218636f0df942ece4f7
Matt Basta <me@mattbasta.com>
2017-09-15 17:03:50 -0700

    WIP

--- packages/renderer/src/jsAPI.ts
+++ packages/renderer/src/jsAPI.ts
@@ -1,5 +1,5 @@
 import * as rendering from './rendering';
-import {route as routesRoute, routesRoutes} from './routes';
+import {route as routesRoute, rroutes as outesRoutes} from './routes';
 
 export default rendering;
 export const route = routesRoute;
