fbc7702ddc1d5f6f5bb8dc214d5d88e4a7e9abbd
Matt Basta <me@mattbasta.com>
2017-11-12 12:12:07 -0800

    See how prettier-ing JS goes

--- packages/presets/src/components/links-linkBar.js
+++ packages/presets/src/components/links-linkBar.js
@@ -1,35 +1,46 @@
-export default ({includes, layout = null, elementOptions = null, style = null}) => ({
-    type: 'links.linkBar',
-    layout: Object.assign({
-        includes,
-        textStyle: {
-            color: 'text',
-        },
-    }, layout),
-    template: {
-        tagName: 'nav',
-        elements: [
-            {
-                type: 'layout.fixedWrapper',
-                children: [
-                    {
-                        type: 'mount',
-                        props: {mount: 'links'},
-                    },
-                ],
-                elementOptions: Object.assign({
-                    bgColor: 'secondaryAccent',
-                    fgColor: 'secondaryAccent',
-                    innerPadding: '40px 0',
-                    maxWidth: 960,
-                }, elementOptions),
-                styles: Object.assign({textAlign: 'left'}, style),
-            },
+export default ({
+  includes,
+  layout = null,
+  elementOptions = null,
+  style = null,
+}) => ({
+  type: 'links.linkBar',
+  layout: Object.assign(
+    {
+      includes,
+      textStyle: {
+        color: 'text',
+      },
+    },
+    layout,
+  ),
+  template: {
+    tagName: 'nav',
+    elements: [
+      {
+        type: 'layout.fixedWrapper',
+        children: [
+          {
+            type: 'mount',
+            props: {mount: 'links'},
+          },
         ],
-    },
+        elementOptions: Object.assign(
+          {
+            bgColor: 'secondaryAccent',
+            fgColor: 'secondaryAccent',
+            innerPadding: '40px 0',
+            maxWidth: 960,
+          },
+          elementOptions,
+        ),
+        styles: Object.assign({textAlign: 'left'}, style),
+      },
+    ],
+  },
 
-    tag: 'links.linkBar',
-    tagMetadata: {
-        includes,
-    },
+  tag: 'links.linkBar',
+  tagMetadata: {
+    includes,
+  },
 });
