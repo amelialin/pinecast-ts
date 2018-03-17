52e7253a33bf6a1462e9c5427afa7c81c490b655
Matt Basta <me@mattbasta.com>
2018-03-17 09:50:27 -0700

    Clean up common buttons

--- packages/sb-editor/src/common/Button.tsx
+++ packages/sb-editor/src/common/Button.tsx
@@ -14,14 +14,12 @@ const NativeButton = styled(
     $isPrimary,
     $pending,
     $size,
-    style,
   }: {
     disabled?: boolean;
     $isBlock?: boolean;
     $isPrimary?: boolean;
     $pending?: boolean;
     $size: 'normal' | 'large' | 'small';
-    style: React.CSSProperties;
   }) => ({
     backgroundColor: $isPrimary ? '#8d52d1' : '#fff',
     border: 0,
@@ -41,8 +39,7 @@ const NativeButton = styled(
       $size === 'normal' ? '0 16px' : $size === 'small' ? '0 12px' : '0 20px',
     pointerEvents: $pending || disabled ? 'none' : null,
     transition: 'box-shadow 0.2s, opacity 0.2s',
-
-    ...style,
+    userSelect: 'none',
 
     ':hover': {
       boxShadow:
@@ -56,6 +53,11 @@ const NativeButton = styled(
     ':focus': {
       boxShadow:
         '0 1px 2px rgba(0, 0, 0, 0.15), 0 3px 4px rgba(0, 0, 0, 0.05), 0 0 0 0.5px rgba(0, 0, 0, .15), rgba(167, 210, 243, 0.75) 0 0 0 2px inset',
+      outline: 'none',
+    },
+    ':hover:focus': {
+      boxShadow:
+        '0 1px 2px rgba(0, 0, 0, 0.15), 0 3px 5px rgba(0, 0, 0, 0.15), 0 0 0 0.5px rgba(0, 0, 0, .15), rgba(167, 210, 243, 0.75) 0 0 0 2px inset',
       outline: 'none',
     },
     ':active:focus': {
