79974c1a2b841ca11b94453175e81d8ba14ef684
Matt Basta <me@mattbasta.com>
2018-04-08 10:57:41 -0700

    Remove some invalid TODOs

--- packages/common/src/Tag.tsx
+++ packages/common/src/Tag.tsx
@@ -20,12 +20,10 @@ const Tag = styled('span', ({$color = 'gray'}: Props) => {
     case 'gray':
       break;
     case 'green':
-      // TODO: Get these colors documented
       out.backgroundColor = '#8aeabf';
       out.color = '#146640';
       break;
     case 'red':
-      // TODO: Get these colors documented
       out.backgroundColor = '#FEDEDE';
       out.color = '#BF1D1D';
       break;
