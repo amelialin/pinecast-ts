0f5d596c1de81896f47128fd80b3a5dc20d67062
Matt Basta <me@mattbasta.com>
2018-08-19 16:23:30 -0700

    Switch import tool to OptionButton

--- packages/db-import-tool/src/components/SourceSelector.tsx
+++ packages/db-import-tool/src/components/SourceSelector.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 
 import Card from '@pinecast/common/Card';
 import {DashboardTitle} from '@pinecast/common/Text';
-import {DEFAULT_FONT} from '@pinecast/common/constants';
 import {MovingTruck, Quill} from '@pinecast/common/icons/medium';
+import OptionButton from '@pinecast/common/OptionButton';
 import styled, {CSS} from '@pinecast/styles';
 
 const cardStyles: CSS = {
   margin: '0 auto',
-  maxWidth: 400,
+  maxWidth: 450,
 
   '@media (max-width: 800px)': {
     maxWidth: 'none',
@@ -19,46 +19,10 @@ const Row = styled('div', {
   display: 'flex',
   justifyContent: 'stretch',
 });
-
-const Option = styled('button', ({$selected}: {$selected: boolean}) => ({
-  alignItems: 'center',
-  appearance: 'none',
-  backgroundColor: $selected ? '#DFEDF3' : '#fff',
-  border: 0,
-  borderRadius: 4,
-  boxShadow: '0 0 0 0 transparent, 0 0 0 0 transparent',
-  cursor: 'pointer',
-  display: 'flex',
-  flex: '1 1',
-  flexDirection: 'column',
-  fontFamily: DEFAULT_FONT,
-  fontSize: 14,
-  margin: '0 4px',
-  padding: 12,
-  transition: 'background-color 0.1s, box-shadow 0.2s',
-
-  ':not(:empty) > svg': {
-    marginBottom: 12,
-  },
-
-  ':first-child': {
-    marginLeft: 0,
-  },
-  ':last-child': {
-    marginRight: 0,
-  },
-
-  ':hover': {
-    backgroundColor: '#DFEDF3',
-  },
-
-  ':focus': {
-    boxShadow: $selected
-      ? '0 1px 2px rgba(0, 0, 0, 0.15), #eee 0 0 0 4px'
-      : '0 0 0 0 transparent, #eee 0 0 0 4px',
-    outline: 'none',
-  },
-}));
+const TextWrap = styled('span', {
+  display: 'block',
+  marginTop: 8,
+});
 
 const SourceSelector = ({
   onChange,
@@ -72,20 +36,25 @@ const SourceSelector = ({
       How would you like to begin?
     </DashboardTitle>
     <Row>
-      <Option onClick={() => onChange('form')} $selected={source === 'form'}>
+      <OptionButton
+        onClick={() => onChange('form')}
+        selected={source === 'form'}
+        style={{alignItems: 'center', flex: '1 1', marginRight: 12}}
+      >
         <Quill color={source === 'form' ? '#4e7287' : '#708d9e'} height={48} />
-        Start from scratch
-      </Option>
-      <Option
+        <TextWrap>Start from scratch</TextWrap>
+      </OptionButton>
+      <OptionButton
         onClick={() => onChange('import')}
-        $selected={source === 'import'}
+        selected={source === 'import'}
+        style={{alignItems: 'center', flex: '1 1'}}
       >
         <MovingTruck
           color={source === 'import' ? '#4e7287' : '#708d9e'}
           height={48}
         />
-        Import from elsewhere
-      </Option>
+        <TextWrap>Import from elsewhere</TextWrap>
+      </OptionButton>
     </Row>
   </Card>
 );
